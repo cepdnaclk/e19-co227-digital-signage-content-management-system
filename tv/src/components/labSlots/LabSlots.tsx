509a4fbf9dcf0d586c4c7671d7754559f0638146
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-09-13 16:37:21 +0530

    labslots done

--- tv/src/components/labSlots/LabSlots.tsx
+++ tv/src/components/labSlots/LabSlots.tsx
@@ -1,8 +1,56 @@
 // LabSlots.tsx
 import React from "react";
-import "./labSlots.css";
+import "./labslots.css";
+import "./slot";
+import Slot from "./slot";
 
 const LabSlots: React.FC = () => {
+  const timeSlots: string[] = [
+    "08.00-09.00",
+    "09.00-10.00",
+    "10.00-11.00",
+    "11.00-12.00",
+    "12.00-13.00",
+    "13.00-14.00",
+    "14.00-15.00",
+    "15.00-16.00",
+    "16.00-17.00",
+  ];
+
+  type allocation = {
+    lab: number;
+    start: string;
+    end: string;
+    name: string;
+  };
+
+  const allocations: allocation[] = [
+    {
+      lab: 1,
+      start: "13.30",
+      end: "17.00",
+      name: "Agri",
+    },
+    {
+      lab: 2,
+      start: "08.00",
+      end: "12.00",
+      name: "Vet",
+    },
+    {
+      lab: 3,
+      start: "10.00",
+      end: "14.55",
+      name: "CCNA",
+    },
+    {
+      lab: 4,
+      start: "10.00",
+      end: "11.00",
+      name: "Agri",
+    },
+  ];
+
   return (
     <div className="labslots">
       <div className="legend">
@@ -26,6 +74,19 @@ const LabSlots: React.FC = () => {
       <div className="date">
         <h3>Today</h3>
         <p>27 Aug 2023</p>
+      </div>
+      <div className="timetable">
+        {timeSlots.map((slot, index) => (
+          <div key={index} className="time-slot">
+            <p>{slot}</p>
+            {index > 0 && (
+              <div className="line" style={{ gridRow: index + 1 }}></div>
+            )}
+          </div>
+        ))}
+        {allocations.map((lab, index) => (
+          <Slot details={lab} key={index} />
+        ))}
       </div>
     </div>
   );
