a8a6d327298e62fb0b12173dffa761a49a2da5fd
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-09-13 16:23:51 +0530

    dot color in corseoffering

--- tv/src/components/labSlots/LabSlots.tsx
+++ tv/src/components/labSlots/LabSlots.tsx
@@ -25,7 +25,7 @@ const LabSlots: React.FC = () => {
       </div>
       <div className="date">
         <h3>Today</h3>
-        <p>27 Aug 2023</p>
+        <p>13 Sep 2023</p>
       </div>
     </div>
   );
