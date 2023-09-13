5b2f583ecd0cb12f3c14549f1a0b5a68723b25fe
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-09-13 08:48:58 +0530

    fix side bar

--- tv/src/components/SideBar.tsx
+++ tv/src/components/SideBar.tsx
@@ -1,20 +1,40 @@
 // SideBar.tsx
 
-import React, { useState } from 'react';
-import './sidebar.css';
-import logo_a from '../assets/logo_a.svg';
-import logo_b from '../assets/logo_b.svg';
+import { useState } from "react";
+import "./sidebar.css";
+import logo_a from "../assets/logo_a.svg";
+import logo_b from "../assets/logo_b.svg";
 
-function Sidebar({ selectedOption, setSelectedOption }) {
-  const [hoveredOption, setHoveredOption] = useState('');
+type SideBarProps = {
+  selectedOption: string;
+  setSelectedOption: (option: string) => void;
+};
+
+function Sidebar({ selectedOption, setSelectedOption }: SideBarProps) {
+  const [hoveredOption, setHoveredOption] = useState<string>("");
 
   // Define your options and their corresponding logos
   const options = [
-    { label: 'Lab Slots', logo: hoveredOption === 'Lab Slots' ? logo_b : logo_a },
-    { label: 'Course Offerings', logo: hoveredOption === 'Course Offerings' ? logo_b : logo_a },
-    { label: 'Upcoming Events', logo: hoveredOption === 'Upcoming Events' ? logo_b : logo_a },
-    { label: 'Previous Events', logo: hoveredOption === 'Previous Events' ? logo_b : logo_a },
-    { label: 'Achievements', logo: hoveredOption === 'Achievements' ? logo_b : logo_a },
+    {
+      label: "Lab Slots",
+      logo: hoveredOption === "Lab Slots" ? logo_b : logo_a,
+    },
+    {
+      label: "Course Offerings",
+      logo: hoveredOption === "Course Offerings" ? logo_b : logo_a,
+    },
+    {
+      label: "Upcoming Events",
+      logo: hoveredOption === "Upcoming Events" ? logo_b : logo_a,
+    },
+    {
+      label: "Previous Events",
+      logo: hoveredOption === "Previous Events" ? logo_b : logo_a,
+    },
+    {
+      label: "Achievements",
+      logo: hoveredOption === "Achievements" ? logo_b : logo_a,
+    },
     // Add other options and logos here...
   ];
 
@@ -24,9 +44,9 @@ function Sidebar({ selectedOption, setSelectedOption }) {
         {options.map((option) => (
           <li
             key={option.label}
-            className={selectedOption === option.label ? 'selected' : ''}
+            className={selectedOption === option.label ? "selected" : ""}
             onMouseEnter={() => setHoveredOption(option.label)} // Change logo on hover
-            onMouseLeave={() => setHoveredOption('')} // Reset logo when mouse leaves
+            onMouseLeave={() => setHoveredOption("")} // Reset logo when mouse leaves
             onClick={() => setSelectedOption(option.label)}
           >
             <img src={option.logo} alt={`${option.label} Logo`} />
