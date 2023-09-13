a7b2b35ca61612406d0a7558af1093ebd2290ba1
KalinduWijerathna <e19446@eng.pdn.ac.lk>
2023-09-13 10:32:58 +0530

    temp changes

--- tv/src/components/labSlots/LabSlots.tsx
+++ tv/src/components/labSlots/LabSlots.tsx
@@ -1,14 +1,20 @@
 // LabSlots.tsx
 import React from "react";
 import "./labSlots.css";
+import img from "../../assets/lab-slots-img.png";
+
 
 const LabSlots: React.FC = () => {
   return (
-    <div className="labslots">
-      <div className="legend">
-        <div className="legend-bit"></div>
-      </div>
-      <div className="date"></div>
+    // <div className="labslots">
+    //   <div className="legend">
+    //     <div className="legend-bit"></div>
+    //   </div>
+    //   <div className="date"></div>
+    // </div>
+
+    <div className="lab-slot-image">
+      <img src={img}/>
     </div>
   );
 };
