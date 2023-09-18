16c85684ed1395cf0ffa8f8adbfbed4f10c695b7
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-09-18 12:41:32 +0530

    cards done

--- admin/pages/course.php
+++ admin/pages/course.php
@@ -4,80 +4,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/card.css">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="../css/course.css">
     <title>IT Center | Courses</title>
 </head>
 
 <body>
     <?php include('../includes/header.php'); ?>
 
-    <div class="content">
-        <h1>Courses</h1>
-        <p>Currently Offered Courses by Us</p>
-    </div>
+    <main class="course">
+        <div class="container">
+            <h1>Courses</h1>
+            <p>Currently Offered Courses by Us</p>
 
-    <div class="card-container">
-        <!-- Card 1 -->
-        <div class="card">
-            <img src="../images/ccna.png" alt="Card Image 1">
-            <div class="card-content">
-                <h2 class="card-title">CCNA</h2>
-                <p class="card-text">Dr. Kumaratunga Munidasa</p>
-            </div>
-            <!-- Card actions with icons -->
-            <div class="card-actions">
-                <button class="edit-button"><span class="icon">&#9998;</span>Edit</button>
-                <button class="delete-button"><span class="icon">&#128465;</span>Delete</button>
-            </div>
+            <div class="card-container">
+                <!-- Card 1 -->
+                <div class="card">
+                    <img src="../images/ccna.png" alt="Card Image 1">
+                    <div class="card-content">
+                        <h2 class="card-title">CCNA</h2>
+                        <p class="card-text">Dr. Kumaratunga Munidasa</p>
+                    </div>
+                    <!-- Card actions with icons -->
+                    <div class="card-actions">
+                        <button class="edit-button"><span class="icon">&#9998;</span>Edit</button>
+                        <button class="delete-button"><span class="icon">&#128465;</span>Delete</button>
+                    </div>
 
+                </div>
+
+                <!-- Card 2 -->
+                <div class="card">
+                    <img src="../images/ccna.png" alt="Card Image 2">
+                    <div class="card-content">
+                        <h2 class="card-title">CCNA</h2>
+                        <p class="card-text">Dr. Don Don</p>
+                    </div>
+                    <!-- Card actions with icons -->
+                    <div class="card-actions">
+                        <button class="edit-button"><span class="icon">&#9998;</span>Edit</button>
+                        <button class="delete-button"><span class="icon">&#128465;</span>Delete</button>
+                    </div>
+
+                </div>
+
+                <!-- Card 3 -->
+                <div class="card">
+                    <img src="../images/ccna.png" alt="Card Image 3">
+                    <div class="card-content">
+                        <h2 class="card-title">CCNA</h2>
+                        <p class="card-text">Dr. Magic Perera</p>
+                    </div>
+                    <!-- Card actions with icons -->
+                    <div class="card-actions">
+                        <button class="edit-button"><span class="icon">&#9998;</span>Edit</button>
+                        <button class="delete-button"><span class="icon">&#128465;</span>Delete</button>
+                    </div>
+
+                </div>
+
+                <!-- Card 4 -->
+                <div class="card">
+                    <img src="../images/ccna.png" alt="Card Image 4">
+                    <div class="card-content">
+                        <h2 class="card-title">CCNA</h2>
+                        <p class="card-text">Dr. A.B. de Great</p>
+                    </div>
+                    <!-- Card actions with icons -->
+                    <div class="card-actions">
+                        <button class="edit-button"><span class="icon">&#9998;</span>Edit</button>
+                        <button class="delete-button"><span class="icon">&#128465;</span>Delete</button>
+                    </div>
+
+                </div>
+            </div>
         </div>
-
-        <!-- Card 2 -->
-        <div class="card">
-            <img src="../images/ccna.png" alt="Card Image 2">
-            <div class="card-content">
-                <h2 class="card-title">CCNA</h2>
-                <p class="card-text">Dr. Don Don</p>
-            </div>
-            <!-- Card actions with icons -->
-            <div class="card-actions">
-                <button class="edit-button"><span class="icon">&#9998;</span>Edit</button>
-                <button class="delete-button"><span class="icon">&#128465;</span>Delete</button>
-            </div>
-
-        </div>
-
-        <!-- Card 3 -->
-        <div class="card">
-            <img src="../images/ccna.png" alt="Card Image 3">
-            <div class="card-content">
-                <h2 class="card-title">CCNA</h2>
-                <p class="card-text">Dr. Magic Perera</p>
-            </div>
-            <!-- Card actions with icons -->
-            <div class="card-actions">
-                <button class="edit-button"><span class="icon">&#9998;</span>Edit</button>
-                <button class="delete-button"><span class="icon">&#128465;</span>Delete</button>
-            </div>
-
-        </div>
-
-        <!-- Card 4 -->
-        <div class="card">
-            <img src="../images/ccna.png" alt="Card Image 4">
-            <div class="card-content">
-                <h2 class="card-title">CCNA</h2>
-                <p class="card-text">Dr. A.B. de Great</p>
-            </div>
-            <!-- Card actions with icons -->
-            <div class="card-actions">
-                <button class="edit-button"><span class="icon">&#9998;</span>Edit</button>
-                <button class="delete-button"><span class="icon">&#128465;</span>Delete</button>
-            </div>
-
-        </div>
-    </div>
+    </main>
 
     <?php include('../includes/footer.php'); ?>
 </body>
