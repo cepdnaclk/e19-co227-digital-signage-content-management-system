ec0f4e519ebf376068b89adda2fd55df90107703
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-09-19 13:11:08 +0530

    sidebar function made

--- admin/pages/course.php
+++ admin/pages/course.php
@@ -13,7 +13,8 @@
     <div class="flex-box">
         <div class="left">
             <?php
-            include_once(APP_ROOT . "/includes/sidebar.php")
+            include_once(APP_ROOT . "/includes/sidebar.php");
+            sidebar(1);
             ?>
         </div>
         <div class="right">
@@ -21,59 +22,59 @@
             include_once(APP_ROOT . "/includes/header.php");
             ?>
             <main class="course">
-        <div class="container">
-            <h1>Courses</h1>
-            <p>Currently Offered Courses by Us</p>
+                <div class="container">
+                    <h1>Courses</h1>
+                    <p>Currently Offered Courses by Us</p>
 
-            <div class="table-container">
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th class="text-left">Course</th>
-                                <th class="text-left">Description</th>
-                                <th class="text-center"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- Sample Rows -->
-                            <tr>
-                                <td class="text-left">CCNA 200-301</td>
-                                <td class="text-left">Cisco Certified Network Associate</td>
-                                <td class="text-center">
-                                    <button class="btn btn-primary">Edit</button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-left">Sample Course</td>
-                                <td class="text-left">Sample Description</td>
-                                <td class="text-center">
-                                    <button class="btn btn-primary">Edit</button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-left">Another Course</td>
-                                <td class="text-left">Another Description</td>
-                                <td class="text-center">
-                                    <button class="btn btn-primary">Edit</button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-left">Yet Another Course</td>
-                                <td class="text-left">Yet Another Description</td>
-                                <td class="text-center">
-                                    <button class="btn btn-primary">Edit</button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div class="table-container">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th class="text-left">Course</th>
+                                        <th class="text-left">Description</th>
+                                        <th class="text-center"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- Sample Rows -->
+                                    <tr>
+                                        <td class="text-left">CCNA 200-301</td>
+                                        <td class="text-left">Cisco Certified Network Associate</td>
+                                        <td class="text-center">
+                                            <button class="btn btn-primary">Edit</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-left">Sample Course</td>
+                                        <td class="text-left">Sample Description</td>
+                                        <td class="text-center">
+                                            <button class="btn btn-primary">Edit</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-left">Another Course</td>
+                                        <td class="text-left">Another Description</td>
+                                        <td class="text-center">
+                                            <button class="btn btn-primary">Edit</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-left">Yet Another Course</td>
+                                        <td class="text-left">Yet Another Description</td>
+                                        <td class="text-center">
+                                            <button class="btn btn-primary">Edit</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="add-course text-center">
+                            <button class="btn btn-success">Add New Course</button>
+                        </div>
+                    </div>
                 </div>
-                <div class="add-course text-center">
-                    <button class="btn btn-success">Add New Course</button>
-                </div>
-            </div>
-        </div>
-    </main>
+            </main>
         </div>
     </div>
 </body>
