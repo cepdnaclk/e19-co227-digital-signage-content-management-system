1eddf44c52542f111737bb4ca826b9f2c59c93b4
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-09-20 09:58:18 +0530

    create add new labslot

---
+++ admin/pages/addnewlabslot.php
@@ -0,0 +1,31 @@
+<?php include_once "../config.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/labslots.css">
+    <title>IT Center | Lab Allocations</title>
+</head>
+
+<body>
+    <div class="flex-box">
+        <div class="left">
+            <?php
+            include_once(APP_ROOT . "/includes/sidebar.php");
+            sidebar(4);
+            ?>
+        </div>
+        <div class="right">
+            <?php
+            include_once(APP_ROOT . "/includes/header.php");
+            ?>
+            <main class="labslots">
+
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
