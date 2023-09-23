2b0b31c44872edd2ea8ad2e4e32e9100533e8c1e
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-09-23 08:20:17 +0530

    fix sql file

--- admin/DB/cmsdb.sql
+++ admin/DB/cmsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 21, 2023 at 12:11 PM
+-- Generation Time: Sep 23, 2023 at 02:49 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 SET
@@ -32,6 +32,190 @@ SET
 --
 -- Database: `cmsdb`
 --
+-- --------------------------------------------------------
+--
+-- Table structure for table `achievement`
+--
+DROP TABLE IF EXISTS `achievement`;
+
+CREATE TABLE IF NOT EXISTS `achievement` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `a_name` varchar(100) DEFAULT NULL,
+  `a_img` varchar(100) NOT NULL,
+  `display_from` date NOT NULL,
+  `display_to` date NOT NULL,
+  `added_by` int(11) NOT NULL COMMENT 'F_key - UserId',
+  PRIMARY KEY (`a_id`),
+  KEY `fk_admin_id` (`added_by`)
+) ENGINE = MyISAM AUTO_INCREMENT = 5 DEFAULT CHARSET = latin1;
+
+--
+-- Dumping data for table `achievement`
+--
+INSERT INTO
+  `achievement` (
+    `a_id`,
+    `a_name`,
+    `a_img`,
+    `display_from`,
+    `display_to`,
+    `added_by`
+  )
+VALUES
+  (
+    1,
+    'Achievement 1',
+    'achievement1.jpg',
+    '2023-07-01',
+    '2023-07-15',
+    1
+  ),
+  (
+    2,
+    'Achievement 2',
+    'achievement2.jpg',
+    '2023-08-01',
+    '2023-08-15',
+    2
+  ),
+  (
+    3,
+    'Achievement 3',
+    'achievement3.jpg',
+    '2023-09-01',
+    '2023-09-15',
+    3
+  ),
+  (
+    4,
+    'Achievement 4',
+    'achievement4.jpg',
+    '2023-10-01',
+    '2023-10-15',
+    4
+  );
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `course`
+--
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE IF NOT EXISTS `course` (
+  `c_id` int(11) NOT NULL AUTO_INCREMENT,
+  `c_name` varchar(50) NOT NULL,
+  `details` text DEFAULT NULL,
+  PRIMARY KEY (`c_id`)
+) ENGINE = MyISAM AUTO_INCREMENT = 5 DEFAULT CHARSET = latin1;
+
+--
+-- Dumping data for table `course`
+--
+INSERT INTO
+  `course` (`c_id`, `c_name`, `details`)
+VALUES
+  (
+    1,
+    'Introduction to Programming',
+    'Basic programming concepts and techniques.'
+  ),
+  (
+    2,
+    'Web Development',
+    'Building web applications using HTML, CSS, and JavaScript.'
+  ),
+  (
+    3,
+    'Database Management',
+    'Database design and SQL.'
+  ),
+  (
+    4,
+    'Networking Fundamentals',
+    'Basics of computer networking.'
+  );
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `course_offering`
+--
+DROP TABLE IF EXISTS `course_offering`;
+
+CREATE TABLE IF NOT EXISTS `course_offering` (
+  `c_code` varchar(20) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `coordinator_id` int(11) NOT NULL,
+  `starting date` date NOT NULL,
+  `display_info` text DEFAULT NULL,
+  PRIMARY KEY (`c_code`),
+  KEY `fk_c_id` (`c_id`),
+  KEY `fk_coord_id` (`coordinator_id`)
+) ENGINE = MyISAM DEFAULT CHARSET = latin1;
+
+--
+-- Dumping data for table `course_offering`
+--
+INSERT INTO
+  `course_offering` (
+    `c_code`,
+    `c_id`,
+    `coordinator_id`,
+    `starting date`,
+    `display_info`
+  )
+VALUES
+  (
+    'COURSE01',
+    1,
+    1,
+    '2023-08-15',
+    'Course details for August 2023'
+  ),
+  (
+    'COURSE02',
+    2,
+    2,
+    '2022-12-10',
+    'Course details for December 2022'
+  ),
+  (
+    'COURSE03',
+    3,
+    3,
+    '2023-09-05',
+    'Course details for September 2023'
+  ),
+  (
+    'COURSE04',
+    4,
+    4,
+    '2023-10-01',
+    'Course details for October 2023'
+  );
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `facility`
+--
+DROP TABLE IF EXISTS `facility`;
+
+CREATE TABLE IF NOT EXISTS `facility` (
+  `f_id` int(11) NOT NULL AUTO_INCREMENT,
+  `f_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`f_id`)
+) ENGINE = MyISAM AUTO_INCREMENT = 5 DEFAULT CHARSET = latin1;
+
+--
+-- Dumping data for table `facility`
+--
+INSERT INTO
+  `facility` (`f_id`, `f_name`)
+VALUES
+  (1, 'Seminar Room'),
+  (2, 'Classroom A'),
+  (3, 'Classroom B'),
+  (4, 'Auditorium');
+
 -- --------------------------------------------------------
 --
 -- Table structure for table `labslot`
@@ -76,7 +260,7 @@ VALUES
     2,
     'Lab1',
     'CourseB',
-    '13:00:00',
+    '13:30:00',
     '15:00:00',
     2,
     NULL
@@ -116,6 +300,212 @@ VALUES
     '17:00:00',
     5,
     '2023-09-23'
+  );
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `previous_event`
+--
+DROP TABLE IF EXISTS `previous_event`;
+
+CREATE TABLE IF NOT EXISTS `previous_event` (
+  `p_id` int(11) NOT NULL AUTO_INCREMENT,
+  `p_name` varchar(100) DEFAULT NULL,
+  `p_img` varchar(100) NOT NULL,
+  `display_from` date NOT NULL,
+  `display_to` date NOT NULL,
+  `added_by` int(11) NOT NULL COMMENT 'f_key User_id',
+  PRIMARY KEY (`p_id`),
+  KEY `fk_admin_id` (`added_by`)
+) ENGINE = MyISAM AUTO_INCREMENT = 5 DEFAULT CHARSET = latin1;
+
+--
+-- Dumping data for table `previous_event`
+--
+INSERT INTO
+  `previous_event` (
+    `p_id`,
+    `p_name`,
+    `p_img`,
+    `display_from`,
+    `display_to`,
+    `added_by`
+  )
+VALUES
+  (
+    1,
+    'Previous Event 1',
+    'prev_event1.jpg',
+    '2022-12-01',
+    '2022-12-15',
+    1
+  ),
+  (
+    2,
+    'Previous Event 2',
+    'prev_event2.jpg',
+    '2022-11-01',
+    '2022-11-15',
+    2
+  ),
+  (
+    3,
+    'Previous Event 3',
+    'prev_event3.jpg',
+    '2022-10-01',
+    '2022-10-15',
+    3
+  ),
+  (
+    4,
+    'Previous Event 4',
+    'prev_event4.jpg',
+    '2022-09-01',
+    '2022-09-15',
+    4
+  );
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `upcoming_event`
+--
+DROP TABLE IF EXISTS `upcoming_event`;
+
+CREATE TABLE IF NOT EXISTS `upcoming_event` (
+  `e_id` int(11) NOT NULL AUTO_INCREMENT,
+  `e_name` varchar(100) DEFAULT NULL,
+  `e_date` date DEFAULT NULL,
+  `e_time` time DEFAULT NULL,
+  `e_venue` varchar(100) DEFAULT NULL,
+  `e_img` varchar(100) NOT NULL,
+  `display_from` date NOT NULL,
+  `display_to` date NOT NULL,
+  `added_by` int(11) NOT NULL COMMENT 'f_key -userId',
+  PRIMARY KEY (`e_id`),
+  KEY `fk_admin_id` (`added_by`)
+) ENGINE = MyISAM AUTO_INCREMENT = 5 DEFAULT CHARSET = latin1;
+
+--
+-- Dumping data for table `upcoming_event`
+--
+INSERT INTO
+  `upcoming_event` (
+    `e_id`,
+    `e_name`,
+    `e_date`,
+    `e_time`,
+    `e_venue`,
+    `e_img`,
+    `display_from`,
+    `display_to`,
+    `added_by`
+  )
+VALUES
+  (
+    1,
+    'Event 1',
+    '2023-09-20',
+    '09:00:00',
+    'Seminar Room',
+    'event1.jpg',
+    '2023-09-10',
+    '2023-09-22',
+    1
+  ),
+  (
+    2,
+    'Event 2',
+    '2023-09-21',
+    '11:00:00',
+    'Auditorium',
+    'event2.jpg',
+    '2023-09-12',
+    '2023-09-24',
+    2
+  ),
+  (
+    3,
+    'Event 3',
+    '2023-09-22',
+    '09:00:00',
+    'Classroom A',
+    'event3.jpg',
+    '2023-09-14',
+    '2023-09-26',
+    3
+  ),
+  (
+    4,
+    'Event 4',
+    '2023-09-23',
+    '15:00:00',
+    'Seminar Room',
+    'event4.jpg',
+    '2023-09-16',
+    '2023-09-28',
+    4
+  );
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `user`
+--
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `u_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `clearense` varchar(20) NOT NULL,
+  PRIMARY KEY (`u_id`)
+) ENGINE = MyISAM AUTO_INCREMENT = 5 DEFAULT CHARSET = latin1;
+
+--
+-- Dumping data for table `user`
+--
+INSERT INTO
+  `user` (
+    `u_id`,
+    `user_name`,
+    `email`,
+    `password`,
+    `contact`,
+    `clearense`
+  )
+VALUES
+  (
+    1,
+    'John Doe',
+    'john@example.com',
+    'hashed_password_1',
+    '1234567890',
+    'Clearance1'
+  ),
+  (
+    2,
+    'Jane Smith',
+    'jane@example.com',
+    'hashed_password_2',
+    '9876543210',
+    'Clearance2'
+  ),
+  (
+    3,
+    'Alice Johnson',
+    'alice@example.com',
+    'hashed_password_3',
+    '5555555555',
+    'Clearance3'
+  ),
+  (
+    4,
+    'Bob Williams',
+    'bob@example.com',
+    'hashed_password_4',
+    '9999999999',
+    'Clearance4'
   );
 
 COMMIT;
