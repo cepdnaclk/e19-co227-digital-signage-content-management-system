feee1ad2b6650f3da9e38af79df50fe3435589e6
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-10-03 22:25:28 +0530

    adding icons

--- tv/src/components/SideBar.tsx
+++ tv/src/components/SideBar.tsx
@@ -2,39 +2,56 @@
 
 import { useState } from "react";
 import "./sidebar.css";
-import { Link } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
+import labslotsImg from "/src/assets/labslotsimg.svg";
+import achivementImg from "/src/assets/Star_fill.svg";
 
 function Sidebar() {
   return (
     <nav className="sidebar">
       <ul>
         <li>
-          <Link to="/labslots">
-            <img src="../assets/logo_a.svg" alt="" />
+          <Link
+            to="/labslots"
+            className={useMatch("/labslots") ? "selected" : ""}
+          >
+            <img src={labslotsImg} alt="" />
             Lab Slot
           </Link>
         </li>
         <li>
-          <Link to="/courses">
-            <img src="../assets/logo_a.svg" alt="" />
+          <Link
+            to="/courses"
+            className={useMatch("/courses") ? "selected" : ""}
+          >
+            <img src={labslotsImg} alt="" />
             Course Offerings
           </Link>
         </li>
         <li>
-          <Link to="/upcoming">
-            <img src="../assets/logo_a.svg" alt="" />
+          <Link
+            to="/upcoming"
+            className={useMatch("/upcoming") ? "selected" : ""}
+          >
+            <img src={labslotsImg} alt="" />
             Upcoming Events
           </Link>
         </li>
         <li>
-          <Link to="/previous">
-            <img src="../assets/logo_a.svg" alt="" />
+          <Link
+            to="/previous"
+            className={useMatch("/previous") ? "selected" : ""}
+          >
+            <img src={labslotsImg} alt="" />
             Previous Events
           </Link>
         </li>
         <li>
-          <Link to="/achivements">
-            <img src="../assets/logo_a.svg" alt="" />
+          <Link
+            to="/achivements"
+            className={useMatch("/achivements") ? "selected" : ""}
+          >
+            <img src={achivementImg} alt="" />
             Achivements
           </Link>
         </li>
