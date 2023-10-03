dd9ae1b0ac28b15f6812d168b43bf75d01f52b75
inesha21 <e19090@eng.pdn.ac.lk>
2023-10-03 14:16:18 +0530

    change add-achievement page

--- admin/pages/addachievement.php
+++ admin/pages/addachievement.php
@@ -24,23 +24,23 @@
                 <h2>Add Achievement</h2>
                 <div class="form-container">
                     <form action="../backend/achievements.php" method="POST" enctype="multipart/form-data">
-                        <label for="e_img">Select an Image:</label>
-                        <input type="file" name="e_img" id="e_img" required>
+                        <label for="a_img">Select an Image:</label>
+                        <input type="file" name="a_img" id="a_img" required>
                         <br>
-                        <label for="e_name">Achievement Title:</label>
-                        <input type="text" name="e_name" id="e_name">
+                        <label for="a_name">Achievement Title:</label>
+                        <input type="text" name="a_name" id="a_name">
                         <br>
-                        <label for="e_desc">Description:</label>
-                        <input type="text" name="e_desc" id="e_desc" style="height: 150px;">
+                        <label for="a_desc">Description:</label>
+                        <input type="text" name="a_desc" id="a_desc" style="height: 150px;">
                         <br>
-                        <label for="e_date">Date:</label>
+                        <label for="a_date">Date:</label>
                         <input type="date" name="e_date" id="e_date">
                         <br>
-                        <!-- <label for="e_time">Time:</label>
-                        <input type="time" name="e_time" id="e_time">
+                        <!-- <label for="a_time">Time:</label>
+                        <input type="time" name="a_time" id="a_time">
                         <br><br>
-                        <label for="e_venue">Venue:</label>
-                        <input type="text" name="e_venue" id="e_venue">
+                        <label for="a_venue">Venue:</label>
+                        <input type="text" name="a_venue" id="a_venue">
                         <br><br>
                         <label for="display_from">Display from:</label>
                         <input type="date" name="display_from" id="display_from" required>
