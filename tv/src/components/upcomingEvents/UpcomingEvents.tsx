5aa51193c1a99ef65bc6355e23fad4ee5a95336b
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-10-06 12:25:12 +0530

    modified upcomingevents tv

--- tv/src/components/upcomingEvents/UpcomingEvents.tsx
+++ tv/src/components/upcomingEvents/UpcomingEvents.tsx
@@ -7,32 +7,86 @@ import img4 from "../../assets/upcoming-event-posters/upcoming-event-4.png";
 import img5 from "../../assets/upcoming-event-posters/upcoming-event-5.jpg";  // Add  images import here
 //Test images
 
-
-
-export default function UpcomingEvents() {
+const images = [img1, img2, img3, img4, img5];
+export default function Upcomingevents() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const images = [img1, img2, img3, img4, img5]; // Add  image paths to this array
+  const [clickedImageIndex, setClickedImageIndex] = useState<number | null>(null);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      // Increment the current image index
-      setCurrentImageIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 5000); // Change the time interval (in milliseconds) as needed
+    const interval = setInterval(() => {
+      if (clickedImageIndex === null) {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }
+    }, 5000); // Change image every 5 seconds (5000 milliseconds)
 
     return () => {
-      // Clean up the timer to avoid memory leaks
-      clearTimeout(timer);
+      clearInterval(interval);
     };
-  }, [currentImageIndex, images]);
+  }, [clickedImageIndex]);
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handleImageClick = (index: number) => {
+    setCurrentImageIndex(index);
+    setClickedImageIndex(null); // Reset clickedImageIndex to null to exit full-screen mode
+  };
 
   return (
-    
-    <div className="upcoming-events">
-      <img src={images[currentImageIndex]} alt={`Upcoming Event ${currentImageIndex + 1}`} />
+    <div className="upcomingevents-container">
+      <div className="image-controls">
+        <button className="left" onClick={handlePrevImage}>&#10094;</button>
+      </div>
+      <div className="center-content">
+        {clickedImageIndex !== null ? (
+          <div>
+            <img
+              className="center-image"
+              src={images[clickedImageIndex]}
+              alt={`upcomingevent ${clickedImageIndex + 1}`}
+              onClick={() => handleImageClick(currentImageIndex)}
+            />
+          </div>
+        ) : (
+          <div>
+            <img
+              className="center-image"
+              src={images[currentImageIndex]}
+              alt={`upcomingevent ${currentImageIndex + 1}`}
+              onClick={() => handleImageClick(currentImageIndex)}
+            />
+          </div>
+        )}
+
+
+      </div>
+      <div className="image-controls">
+        <button className="right" onClick={handleNextImage}>&#10095;</button>
+      </div>
+      <div className="image-list">
+        {images.map((image, index: number) => (
+          <div
+            key={index}
+            className={`image-item ${
+              currentImageIndex === index || clickedImageIndex === index
+                ? "active"
+                : ""
+            }`}
+            onClick={() => handleImageClick(index)}
+          >
+            <img src={image} alt={`upcomingevent ${index + 1}`} />
+          </div>
+        ))}
+      </div>
     </div>
-    
   );
 }
-
