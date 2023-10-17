6fee46813a94cbf4bc24c881ad342930a2c12649
KalinduWijerathna <e19446@eng.pdn.ac.lk>
2023-10-17 17:26:23 +0530

    modify contact and support

--- admin/backend/contactNsupport.php
+++ admin/backend/contactNsupport.php
@@ -1,5 +1,5 @@
 <<?php
-include_once "../config.php";
+include_once "/config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
--- admin/pages/contactNsupport.php
+++ admin/pages/contactNsupport.php
@@ -1,5 +1,5 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . "/config.php" ;
-include (APP_ROOT . "/includes/header.php");
+include (APP_ROOT . "/includes/header_contactNSupport.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
