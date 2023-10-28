43349e8af2c73aaf85aa33ea2638da5150aa6d62
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-10-28 14:40:56 +0530

    adding clearence

--- admin/backend/api/users/add.php
+++ admin/backend/api/users/add.php
@@ -1,7 +1,9 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . "/config.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/backend/functions/users.php";
-
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     hasClearence(1, function () {
         global $clearenceStatus;
--- admin/backend/api/users/edit.php
+++ admin/backend/api/users/edit.php
@@ -3,6 +3,10 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/config.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/backend/functions/users.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SESSION['user_id'] != $_POST['u_id']) {
+        header("Location: /pages/users/?error='You are not authorize for this'");
+        exit();
+    }
     $result = editUser($_POST['u_id'], $_POST['email'], $_POST['contact']);
     if (isset($result['error'])) {
         header("Location: /pages/users/?error={$result['error']}");
