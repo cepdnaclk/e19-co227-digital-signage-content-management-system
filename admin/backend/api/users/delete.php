0786d4320e0deff1c22eb5dd9d090492038f1187
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-10-28 14:40:32 +0530

    change get to delete and add clearence

---
+++ admin/backend/api/users/delete.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'] . "/backend/functions/users.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    hasClearence(1, function () {
+        global $clearenceStatus;
+
+        if (isset($_GET['delete'])) {
+            $user = getUser($_GET['delete']);
+            if ($user == false) {
+                header('Location: /pages/users/');
+                exit();
+            }
+            if ($clearenceStatus[$user['clearense']] < $clearenceStatus[$_SESSION['clearense']]) {
+                if (deleteUser($_GET['delete'])) {
+                    header('Location: /pages/users/?success=succesfully deleted the user');
+                } else {
+                    header('Location: /pages/users/?error=Unable to delete the user');
+                }
+            }
+        }
+    });
+}
