942aa6a1b7017b9c4f8534be9e054ef179048bfc
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-10-28 14:40:15 +0530

    adding changing pass only for user

--- admin/backend/api/users/changepass.php
+++ admin/backend/api/users/changepass.php
@@ -3,6 +3,11 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/config.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/backend/functions/users.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SESSION['user_id'] != $_POST['u_id']) {
+        header("Location: /pages/users/?error='You are not authorize for this'");
+        exit();
+    }
+
     $result = changePass($_POST['old_password'], $_POST['password'], $_POST['confirm_password'], $_POST['u_id']);
     if (isset($result['error'])) {
         header("Location: /pages/users/?error={$result['error']}");
