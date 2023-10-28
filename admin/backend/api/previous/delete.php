6d2e3d23f37c7cff544509b429c3b1b124041f28
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-10-28 14:11:02 +0530

    adding has clearence to previous events

--- admin/backend/api/previous/delete.php
+++ admin/backend/api/previous/delete.php
@@ -2,11 +2,13 @@
 include_once $_SERVER['DOCUMENT_ROOT'] . "/backend/functions/previousevents.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $event_id = $_GET["delete_id"];
-    $result = deletePreviousEvent($event_id);
-    if ($result) {
-        header("Location: /pages/previous/?success=successfully added lab slot=1");
-    } else {
-        header("Location: /pages/previous/?error=$result");
-    }
+    hasClearence(1, function () {
+        $event_id = $_GET["delete_id"];
+        $result = deletePreviousEvent($event_id);
+        if ($result) {
+            header("Location: /pages/previous/?success=successfully added lab slot=1");
+        } else {
+            header("Location: /pages/previous/?error=$result");
+        }
+    });
 }
--- admin/backend/api/previous/publish.php
+++ admin/backend/api/previous/publish.php
@@ -3,13 +3,15 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/config.php";
 include_once $_SERVER['DOCUMENT_ROOT'] . "/backend/functions/previousevents.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    // Get the event ID from the form submission
-    $event_id = $_GET["publish_id"];
-    $result = publishPreviousEvent($event_id);
+    hasClearence(1, function () {
+        // Get the event ID from the form submission
+        $event_id = $_GET["publish_id"];
+        $result = publishPreviousEvent($event_id);
 
-    // Execute the update query
-    if (isset($result['error'])) {
-        header("Location: /pages/previous/?error={$result['error']}");
-    } else
-        header("Location: /pages/previous/?success={$result['message']}");
+        // Execute the update query
+        if (isset($result['error'])) {
+            header("Location: /pages/previous/?error={$result['error']}");
+        } else
+            header("Location: /pages/previous/?success={$result['message']}");
+    });
 }
