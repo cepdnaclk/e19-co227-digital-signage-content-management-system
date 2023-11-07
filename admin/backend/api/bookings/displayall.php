527f319d6f1d36bddbdbedbf9fd48062389b06ca
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-11-07 20:29:36 +0530

    Debug displayall backend

--- admin/backend/api/bookings/displayall.php
+++ admin/backend/api/bookings/displayall.php
@@ -11,20 +11,22 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     hasClearence(1, function () {
         // Get form data
         $f_id = $_GET["f_id"];
-        $result = displayall($f_id);
+        $resultarr = displayall($f_id);
 
         if (isset($result['error'])) {
-            header("Location: /pages/bookings/?error={$result['error']}");
+            header("Location: /pages/bookings/?error={$resultarr['error']}");
         } else {
 
     // Serialize the $result array into a JSON string
-    $result_json = json_encode($result);
+    $result_encoded = json_encode($resultarr);
 
     // Encode the JSON string to make it safe for inclusion in the URL
-    $result_encoded = urlencode($result_json);
+    $result_encoded = urlencode($result_encoded);
 
     // Include the encoded JSON string as a query parameter
-    header("Location: /pages/bookings/displayall.php?result=$result_encoded");
+    // print_r($resultarr);
+    // print_r($result_encoded);//Debug
+    header("Location: /pages/bookings/displayall.php?resultencoded=$result_encoded");
 }
     });
     exit();
