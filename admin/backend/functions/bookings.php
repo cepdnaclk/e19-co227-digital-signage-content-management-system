3b649e08d7219eea0fa1c3c9f31654dfdd9aff5c
Geethalwickramasingha <e19436@eng.pdn.ac.lk>
2023-11-10 20:08:28 +0530

    modify view of buttons in bookings

--- admin/backend/functions/bookings.php
+++ admin/backend/functions/bookings.php
@@ -53,3 +53,68 @@ function displayall(int $f_id)
 
     return $result;
 }
+
+function getBookingById(int $b_id) {
+    global $conn;
+
+    $sql = 'SELECT * FROM booking WHERE b_id = ?';
+    $stmt = mysqli_prepare($conn, $sql);
+
+    if (!$stmt) {
+        return array('error' => mysqli_error($conn));
+    }
+
+    mysqli_stmt_bind_param($stmt, 'i', $b_id);
+
+    
+    mysqli_stmt_execute($stmt);
+
+    
+    $res = mysqli_stmt_get_result($stmt);
+
+    $result = array();
+
+    if ($res) {
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            $result = $row;
+        } else {
+            $result = array('error' => 'No booking found with the given ID');
+        }
+    } else {
+        $result = array('error' => mysqli_error($conn));
+    }
+
+    // Close the statement
+    mysqli_stmt_close($stmt);
+
+    return $result;
+}
+
+
+function editBooking($b_id, $b_date, $b_timeslot, $b_seats,$b_for,$b_contact, $b_by){
+    global $conn;
+    if(!is_numeric($b_seats) || !is_numeric($b_by) ){
+        return false;
+    }
+    else{
+    $result = array();
+
+
+    $sql = "UPDATE booking SET b_date= ?, b_timeslot = ?, b_seats = ?, b_for = ? ,b_contact = ?, b_by = ? WHERE b_id = ?";
+
+    $stmt = mysqli_prepare($conn, $sql);
+    
+    // Bind parameters
+    mysqli_stmt_bind_param($stmt, "ssissii", $b_date, $b_timeslot, $b_seats,$b_for,$b_contact, $b_by,$b_id);
+
+    if (mysqli_stmt_execute($stmt)) {
+        $result = array('message' => "Booking Updated Successfully");
+    } else {
+        $result = array('error' => mysqli_error($conn));
+    }
+    mysqli_stmt_close($stmt);
+
+    return $result;
+}
+}
