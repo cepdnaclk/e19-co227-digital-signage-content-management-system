32ef3805e4bb2a99ab6bdab13a4ad420c9eeda55
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-01-14 15:47:04 +0530

    adding loading

--- tv/src/context/TimingContextProvider.tsx
+++ tv/src/context/TimingContextProvider.tsx
@@ -5,15 +5,18 @@ import TimingContext from "./TimingContext";
 export function TimingProvider({ children }) {
   const [timings, setTimings] = React.useState({});
   const [imageIndex, setImageIndex] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
     axios
       .get(`/backend/api/dashboard/get.php`)
       .then((res) => {
         setTimings(res.data.features);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   }, [imageIndex]);
 
@@ -23,6 +26,10 @@ export function TimingProvider({ children }) {
     imageIndex,
     setImageIndex,
   };
+
+  if (loading) {
+    return <div>Loading...</div>; // or your own loading component
+  }
 
   return (
     <TimingContext.Provider value={value}>{children}</TimingContext.Provider>
