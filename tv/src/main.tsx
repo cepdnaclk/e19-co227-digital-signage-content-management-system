f22d0ab720c9c2342dbae90f7055c9832ceee64a
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-01-22 22:07:34 +0530

    fixing files and adding dist folder

--- tv/src/main.tsx
+++ tv/src/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-axios.defaults.baseURL = "http://192.168.111.76:8000";
+axios.defaults.baseURL = "http://192.248.43.4";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
