4dd2e2c4d525d6f1f97c7c8669b1c31faebef7ff
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-03-01 08:10:57 +0530

    fix

--- admin/pages/preview/index.php
+++ admin/pages/preview/index.php
@@ -20,14 +20,14 @@
     background-color: #305fa6;
   color: #fff;
   border: none;
-  margin-top: 20px;
   padding: 6px 12px;
   border-radius: 3px;
   cursor: pointer;
   font-weight: bold;
-  margin-bottom: 2rem;
+  margin-bottom: 1rem;
   text-decoration:none;
-  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  width:fit-content;">
         << Go back</a>
             <iframe src="/tv/" width="3810" height="2160" frameborder="0" style="scale:calc(0.3);transform-origin: 0 0;margin:auto;display:block;"></iframe>
 </body>
