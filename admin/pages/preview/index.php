c2cf95a115daa4fe0b84dcdd0ef66063f2ee9080
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-03-01 08:00:59 +0530

    fix preview

--- admin/pages/preview/index.php
+++ admin/pages/preview/index.php
@@ -6,7 +6,7 @@
     <title>Website Viewer</title>
 </head>
 <body style="overflow:hidden">
-    <a href="../" style="padding:0.5rem 1rem; background:var(--color-3); color:white; margin-bottom:1rem;"><< Go back</a>
-    <iframe src="/tv/" width="3810" height="2160" frameborder="0" style="scale:calc(0.3);transform-origin: 0 0;margin:auto;"></iframe>
+    <a href="../" style="padding:0.5rem 1rem; background:var(blue);font-size:1.3rem; color:white; margin-bottom:1rem;"><< Go back</a>
+    <iframe src="/tv/" width="3810" height="2160" frameborder="0" style="scale:calc(0.3);transform-origin: 0 0;margin:auto;display:block;"></iframe>
 </body>
 </html>
