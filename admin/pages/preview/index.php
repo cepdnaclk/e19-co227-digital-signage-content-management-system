fb9b5435c941aed72ea0fc5d5b624e2debd0aa2a
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-03-01 08:05:41 +0530

    fix preview

--- admin/pages/preview/index.php
+++ admin/pages/preview/index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Viewer</title>
 </head>
+
 <body style="overflow:hidden">
-    <a href="../" style="padding:0.5rem 1rem; background:var(blue);font-size:1.3rem; color:white; margin-bottom:1rem;"><< Go back</a>
-    <iframe src="/tv/" width="3810" height="2160" frameborder="0" style="scale:calc(0.3);transform-origin: 0 0;margin:auto;display:block;"></iframe>
+    <a href="../" style="
+    background-color: #305fa6;
+  color: #fff;
+  border: none;
+  margin-top: 20px;
+  padding: 6px 12px;
+  border-radius: 3px;
+  cursor: pointer;
+  font-weight: bold;
+  margin-bottom: 2rem;
+  text-decoration:none;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
+        << Go back</a>
+            <iframe src="/tv/" width="3810" height="2160" frameborder="0" style="scale:calc(0.3);transform-origin: 0 0;margin:auto;display:block;"></iframe>
 </body>
+
 </html>
