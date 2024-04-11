46f4f524afee0278a5763f57d07d1573b3d42702
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-04-11 12:02:27 +0530

    made functions of topics

--- admin/backend/functions/topic.php
+++ admin/backend/functions/topic.php
@@ -1,23 +1,93 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . "/config.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/config.php"; // Include your database connection
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-function getTopics()
+include_once $_SERVER['DOCUMENT_ROOT'] . '/backend/functions/boards.php';
+
+function getTopics(int $board_id)
 {
     global $conn;
 
+    $stmt = $conn->prepare("SELECT * FROM topics WHERE board_id = ?");
+
+    $stmt->bind_param('i', $board_id);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
     $topics = array();
-
-    $result = $conn->query("SELECT topics FROM topics");
-
     while ($row = $result->fetch_assoc()) {
         $topics[] = $row;
     }
 
+    $stmt->close();
+
     return $topics;
 }
 
-function getTopicCount()
+function getTopicCount(int $board_id)
 {
-    $topics = getTopics();
-    return count($topics);
+    if (isAdmin($board_id)) {
+        $topics = getTopics($board_id);
+        return count($topics);
+    }
+    return array("error" => "You are not admin of this board");
+}
+
+function getTopicBoard(int $topic_id)
+{
+    global $conn;
+
+    $stmt = $conn->prepare("SELECT board_id FROM topics WHERE topic_id = ?");
+
+    $stmt->bind_param('i', $topic_id);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+
+    $topic = $result->fetch_assoc();
+
+    $stmt->close();
+
+    return $topic;
+}
+
+function addTopic(int $board_id, string $title, string $icon)
+{
+    if (isAdmin($board_id)) {
+        global $conn;
+
+        $stmt = $conn->prepare("INSERT INTO topics (board_id, title, icon) VALUES (?, ?, ?)");
+        $stmt->bind_param('iss', $board_id, $title, $icon);
+
+        if (!$stmt->execute())
+            return array("error" => $stmt->error);
+        else
+            return array("message" => "Add Topic Succesfully");
+    }
+
+    return array("error" => "You are not admin of this board");
+}
+
+function deleteTopic(int $topic_id)
+{
+    $board_id = getTopicBoard($topic_id);
+
+    if (isAdmin($board_id)) {
+        global $conn;
+
+        $stmt = $conn->prepare("DELETE FROM topics WHERE topic_id = ?");
+        $stmt->bind_param('i', $topic_id);
+
+        if (!$stmt->execute())
+            return array("error" => $stmt->error);
+        else
+            return array("message" => "Delete Topic Succesfully");
+    }
+
+    return array("error" => "You are not admin of this board");
 }
