13ac19c75ef74b5d1581c3ebf7842281df52c4fe
Kavishkagaya <kavishkarambukwella@gmail.com>
2024-04-23 13:23:36 +0530

    add favicon

--- admin/includes/header_core.php
+++ admin/includes/header_core.php
@@ -4,6 +4,7 @@
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -13,6 +14,19 @@
 
 <body>
 
+    <script>
+        const setFavicon = () => {
+            const favicon = document.querySelector('link[rel="icon"]');
+            favicon.href = (window.matchMedia('(prefers-color-scheme: dark)').matches) ?
+                '/images/faviconl.ico' :
+                '/images/favicon.ico';
+        };
+
+        window
+            .matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', setFavicon);
+        setFavicon();
+    </script>
 </body>
 
 </html>
